f6a525d2c88f22d64c9f3e7d49bac3c45e656c14
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-02-19 12:39:31 +0530

    Leaves Controllers

---
+++ hr-web-app/application/models/work-offs/Casual_Leaves.php
@@ -0,0 +1,25 @@
+<?php
+
+class Casual_Leaves extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function get(){
+
+	}
+
+	public function add(){
+
+	}
+
+	public function remove(){
+		
+	}
+
+	public function update(){
+
+	}
+}

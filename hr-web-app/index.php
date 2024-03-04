52df8b64e282acb2bd8e05229b5bc5c66d3e69a5
Hemant Karekar <hemant.v.karekar@gmail.com>
2024-03-04 17:54:26 +0530

    Production Changes

--- hr-web-app/index.php
+++ hr-web-app/index.php
@@ -53,9 +53,9 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	date_default_timezone_set('Asia/Calcutta');
-/*;
+	define('ENVIRONMENT', 'development');
+
+/*
  *---------------------------------------------------------------
  * ERROR REPORTING
  *---------------------------------------------------------------
